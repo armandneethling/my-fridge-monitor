aab2f9471bac6a9abecd078435349ab0411c8c48
Armand Neethling <20231315@ctucareer.co.za>
2025-04-06 00:21:44 +0200

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,51 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
+import { provideHttpClient } from '@angular/common/http';
 
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp, provideFirebaseApp, getApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { firebaseConfig } from './environments/firebase';
 
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { routes } from './app/app.routes';
+import { IonicModule } from '@ionic/angular';
 
-initializeApp(firebaseConfig);
-getAnalytics();
+interface ImportMetaEnv {
+  readonly PROD: boolean;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare global {
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+}
+
+if (import.meta.env.PROD) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideHttpClient(),
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
   ],
-});
+  ...IonicModule.forRoot().providers,
+}).catch((err) => console.error(err));
