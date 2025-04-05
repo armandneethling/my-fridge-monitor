401133acc14bb5de9feb6ba5600bc8c2a8e817d7
Armand Neethling <20231315@ctucareer.co.za>
2025-04-05 09:29:19 +0200

    modified:   src/app/app.routes.ts 	modified:   src/app/home/home.page.html 	modified:   src/app/home/home.page.ts 	new file:   src/app/log/log.page.html 	new file:   src/app/log/log.page.scss 	new file:   src/app/log/log.page.spec.ts 	new file:   src/app/log/log.page.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
 
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'log',
+    loadComponent: () => import('./log/log.page').then( m => m.LogPage)
   },
 ];
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
+  selectedFridge: string = '';
+  temperature: number | null = null;
+
   constructor() {}
+
+  logTemperature() {
+    const now = new Date();
+    const timestamp = now.toISOString(); // Get ISO 8601 timestamp (includes date and time)
+
+    console.log('Selected Fridge:', this.selectedFridge);
+    console.log('Temperature:', this.temperature);
+    console.log('Timestamp:', timestamp);
+
+    // Here, we will later send this data (selectedFridge, temperature, timestamp) to Firebase
+  }
 }
