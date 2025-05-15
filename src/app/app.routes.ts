1ee155f5a0f615885323df64912749459ba1df95
SlothyKoala <aneethling21@gmail.com>
2025-05-15 18:02:59 +0200

    modified:   src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,30 @@ import { HomePage } from './home/home.page';
 import { LogPage } from './log/log.page';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
-  { path: 'home', component: HomePage },
-  { path: 'log', component: LogPage },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '', component: HomePage, pathMatch: 'full' }
+  {
+    path: 'home',
+    component: HomePage,
+    canActivate: [AuthGuard],  // Protect HomePage
+  },
+  {
+    path: 'log',
+    component: LogPage,
+    canActivate: [AuthGuard],  // Protect LogPage
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
