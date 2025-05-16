12ddf611119f7ac45190d41b63cb9c128a07d6e0
SlothyKoala <aneethling21@gmail.com>
2025-05-16 18:25:30 +0200

    modified:   .vscode/settings.json 	modified:   android/app/build.gradle 	modified:   src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './home/home.page';
 import { LogPage } from './log/log.page';
-// import { LoginComponent } from './auth/login/login.component';
-import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomePage,
-    // canActivate: [AuthGuard],
   },
   {
     path: 'log',
     component: LogPage,
-    // canActivate: [AuthGuard],
   },
-  // {
-  //   path: 'login',
-  //   component: LoginComponent,
-  // },
   {
     path: '',
     redirectTo: 'home',
