966c898628d422b0341c37588fc8a6320fd55840
Armand Neethling <110594927+armandneethling@users.noreply.github.com>
2025-06-04 11:11:51 +0200

    Update firebase.ts

--- src/environments/firebase.ts
+++ src/environments/firebase.ts
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "AIzaSyDHggClyHqb3iRfJFQxG91-jen8d5eKbog",
   authDomain: "myfridgemonitordb.firebaseapp.com",
@@ -16,6 +11,5 @@ export const firebaseConfig = {
   measurementId: "G-54LMYYQMPS"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
